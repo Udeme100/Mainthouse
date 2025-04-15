1bb7efbc461dac5baa29b3cd8fc0d46c4e5cc1fe
Udeme100 <udemx200@gmail.com>
2025-04-15 18:07:04 +0100

    Overhauling of the whole website for better user experience

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+// JavaScript to Toggle the Mobile Menu
+function toggleMenu() {
+    const mobileMenu = document.getElementById('mobileMenu');
+    if (mobileMenu.style.display === 'block') {
+        mobileMenu.style.display = 'none'; // Hide the mobile menu
+    } else {
+        mobileMenu.style.display = 'block'; // Show the mobile menu
+    }
+}
+
+
 // Example: Smooth Scroll for links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
