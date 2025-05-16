c44a68d5ab36cfab6a4ee0bbf8fdb311a78ea4b1
Udeme100 <udemx200@gmail.com>
2025-05-16 22:44:32 +0100

    Made the get a quote button functional

--- index.js
+++ index.js
@@ -125,4 +125,7 @@ function setupReviewSectionModal() {
     document.body.classList.remove('modal-open');
     modalImg.src = '';
   }
+  document.querySelector('.cta-button').addEventListener('click', function() {
+    window.location.href = 'mailto:mainthousecleaners@gmail.com?subject=Request%20for%20Quote';
+  });
 }
